aac619a756e4be1e4046eef83667077323a0b18b
Dimitris Kouskousis <web@dkouskousis.gr>
2026-09-20 19:15:44 +0300

    Bump integration version to v0.4.2

--- custom_components/watering_manager/entity.py
+++ custom_components/watering_manager/entity.py
@@ -38,7 +38,7 @@ class WateringManagerEntity(Entity):
             name=system.get("name", "Watering system"),
             manufacturer="Watering Manager",
             model="Managed irrigation zone",
-            sw_version="0.4.1",
+            sw_version="0.4.2",
         )
 
     async def async_added_to_hass(self) -> None:
